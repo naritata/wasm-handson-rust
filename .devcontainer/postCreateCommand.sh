e6031b2d924fe961c2cc1a373070a07f5ad381bb
naritata <narita.takeshi@optimind.tech>
2024-12-05 12:47:40 +0000

    build: devcontainer環境を追加

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,16 @@
+## コンテナで使いたいaliasを設定する
+cp .devcontainer/.bash_aliases ~/.bash_aliases
+
+## ホスト側で使っていて、コンテナでも使いたいsshの設定をコピーする
+## .devcontainer/.ssh/配下に予めconfigや秘密鍵を配置しておくこと
+cp .devcontainer/.ssh/* ~/.ssh/
+
+curl https://get.wasmer.io -sSfL | sh
+source /home/vscode/.wasmer/wasmer.sh
+wasmer --version
+
+rustup -V
+cargo -V
+rustup target add wasm32-unknown-unknown
+rustup target add wasm32-wasi
+cargo install wasm-pack
